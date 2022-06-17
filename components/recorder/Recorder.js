446ec0626299dd122ff7dfb881b68858ae7c494f
screamuch <peterchudinov@icloud.com>
2022-06-16 23:25:04 -0700

    fix axios and json in post request

--- components/recorder/Recorder.js
+++ components/recorder/Recorder.js
@@ -4,8 +4,12 @@ import { Button, StyleSheet, Text, View } from "react-native";
 import { Audio } from "expo-av";
 import * as Sharing from "expo-sharing";
 import { RecordingOptions } from "expo-av/build/Audio";
-import { Axios } from "axios";
+// import { Axios } from "axios";
+import axios, * as others from 'axios'; // correct way to import axios
 import base64 from "react-native-base64";
+
+const baseURL = "http://192.168.1.6:5000"
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -117,9 +121,9 @@ export default function Recorder() {
 
     const audioBase64 = await blobToBase64(blob);
 
-    const response = await Axios.post(
-      "http://192.18.0.4:8000/audio",
-      base64.encode(audioBase64)
+    const response = await axios.post(
+      `${baseURL}/api/transcribe`,
+      { 'audio': base64.encode(audioBase64) }
     );
 
     // We're done with the blob and file uploading, close and release it
