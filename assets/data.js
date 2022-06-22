2974870d75d3605458e492ee6439384b5f2edec6
MedNoun <mouhasahss@gmail.com>
2022-06-22 19:28:13 +0100

    solving modifier problem

---
+++ assets/data.js
@@ -0,0 +1,47 @@
+export var performance_data = {
+  timesig: "4/4",
+  notes: [
+    {
+      measure: 0,
+      pitch_spelled: "C#4",
+      onset_time: 0,
+      length: "h",
+      note_type: "incorrect",
+    },
+    {
+      measure: 0,
+      pitch_spelled: "B4",
+      onset_time: 5,
+      length: "q",
+      note_type: "rest",
+    },
+    {
+      measure: 0,
+      pitch_spelled: "G4",
+      onset_time: 7,
+      length: "8",
+      note_type: "reference",
+    },
+    {
+      measure: 0,
+      pitch_spelled: "C#4",
+      onset_time: 7,
+      length: "8",
+      note_type: "reference",
+    },
+    {
+      measure: 0,
+      pitch_spelled: "E4",
+      onset_time: 7,
+      length: "8",
+      note_type: "extra",
+    },
+    {
+      measure: 0,
+      pitch_spelled: "C4",
+      onset_time: 4,
+      length: "8",
+      note_type: "missing",
+    },
+  ],
+};
--- components/sheet-music/Sheet.js
+++ components/sheet-music/Sheet.js
@@ -2,68 +2,53 @@ import { useScore } from "react-native-vexflow"; // import the one and only thin
 import React from "react";
 import { View } from "react-native";
 import Vex from "vexflow";
+import { performance_data } from "../../assets/data";
 
 export default function Sheet() {
   const [context, stave] = useScore({
-    contextSize: { x: 300, y: 300 }, // this determine the canvas size
-    staveOffset: { x: 5, y: 5 }, // this determine the starting point of the staff relative to top-right corner of canvas
-    staveWidth: 500, // ofc, stave width
+    contextSize: { x: 1000, y: 1000 }, // canvas size
+    staveOffset: { x: 5, y: 5 }, // starting point of the staff relative to the top-right corner of canvas
+    staveWidth: 400, // ofc, stave width
     clef: "treble", // clef
-    timeSig: "4/4", // time signiture
+    timeSig: performance_data.timesig, // time signiture
   });
-
-  // you got your context, you got your stave, you can do your stuff now
-
-  // picked from Vexflow tutorial: https://github.com/0xfe/vexflow/wiki/The-VexFlow-Tutorial
-  const VF = Vex.Flow;
+  // We have our context and stave. Now we add notes to it.
+  const { Renderer, Stave, StaveNote, Voice, Formatter, Accidental, BarNote } =
+    Vex.Flow;
   var notes = [
-    // A quarter-note C.
-    new VF.StaveNote({ clef: "treble", keys: ["c/4"], duration: "q" }),
-
-    // A quarter-note D.
-    new VF.StaveNote({ clef: "treble", keys: ["d/4"], duration: "q" }),
-    // A quarter-note rest. Note that the key (b/4) specifies the vertical
-    // position of the rest.
-    new VF.StaveNote({ clef: "treble", keys: ["b/4"], duration: "qr" }),
-
-    // A C-Major chord.
-    new VF.StaveNote({
-      clef: "treble",
-      keys: ["c/4", "e/4", "g/4"],
-      duration: "q",
+    new StaveNote({
+      keys: ["c#/4"],
+      duration: "h",
+    }).setKeyStyle(0, {
+      fillStyle: "red",
+    }),
+    new BarNote(),
+    new StaveNote({
+      keys: ["c/4"],
+      duration: "8",
+    }).setKeyStyle(0, {
+      fillStyle: "blue",
+    }),
+    new StaveNote({
+      keys: ["b/4"],
+      duration: "qr",
+    }),
+    new StaveNote({
+      keys: ["C#/4", "e/4", "g/4"],
+      duration: "8",
+    })
+      .setKeyStyle(1, {
+        fillStyle: "yellow",
+      })
+      .addModifier(0, new Accidental("#")),
+    new StaveNote({
+      keys: ["C/4", "e/4", "g/4"],
+      duration: "8",
+    }).setKeyStyle(1, {
+      fillStyle: "yellow",
     }),
   ];
-  var notes2 = [
-    // A quarter-note C.
-    new VF.StaveNote({ clef: "treble", keys: ["a/4"], duration: "q" }),
-
-    // A quarter-note D.
-    new VF.StaveNote({ clef: "treble", keys: ["a/4"], duration: "q" }),
-    // A quarter-note rest. Note that the key (b/4) specifies the vertical
-    // position of the rest.
-    new VF.StaveNote({ clef: "treble", keys: ["a/4"], duration: "qr" }),
-
-    // A C-Major chord.
-    new VF.StaveNote({
-      clef: "treble",
-      keys: ["c/4", "e/4", "a/4"],
-      duration: "q",
-    }),
-  ];
-
-  // Create a voice in 4/4 and add the notes from above
-  var voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
-  voice.addTickables(notes);
-  var voice2 = new VF.Voice({ num_beats: 4, beat_value: 4 });
-  voice2.addTickables(notes2);
-
-  // Format and justify the notes to 200 pixels.
-  var formatter = new VF.Formatter()
-    .joinVoices([voice, voice2])
-    .format([voice, voice2], 200);
-
   // Render voice
-  voice.draw(context, stave);
-
+  Formatter.FormatAndDraw(context, stave, notes);
   return <View>{context.render()}</View>;
 }
