422c4a3799a4ecf581b52574fabf69e8a09fe04c
MedNoun <mouhasahss@gmail.com>
2022-06-23 20:13:04 +0100

    adding joe function

--- components/sheet-music/Sheet.js
+++ components/sheet-music/Sheet.js
@@ -1,54 +1,101 @@
-import { useScore } from "react-native-vexflow"; // import the one and only thing provided
+import { useScore } from "react-native-vexflow";
 import React from "react";
 import { View } from "react-native";
 import Vex from "vexflow";
-import { performance_data } from "../../assets/data";
+import data from "../../assets/data";
 
 export default function Sheet() {
   const [context, stave] = useScore({
-    contextSize: { x: 1000, y: 1000 }, // canvas size
+    contextSize: { x: 300, y: 300 }, // canvas size
     staveOffset: { x: 5, y: 5 }, // starting point of the staff relative to the top-right corner of canvas
-    staveWidth: 400, // ofc, stave width
+    staveWidth: 250, // ofc, stave width
     clef: "treble", // clef
-    timeSig: performance_data.timesig, // time signiture
+    timeSig: data.timesig, // time signiture
   });
+
   // We have our context and stave. Now we add notes to it.
   const { Renderer, Stave, StaveNote, Voice, Formatter, Accidental, BarNote } =
     Vex.Flow;
-  var notes = [
-    new StaveNote({
-      keys: ["c#/4"],
-      duration: "h",
-    }).setKeyStyle(0, {
-      fillStyle: "red",
-    }),
-    new BarNote(),
-    new StaveNote({
-      keys: ["c/4"],
-      duration: "8",
-    }).setKeyStyle(0, {
-      fillStyle: "blue",
-    }),
-    new StaveNote({
-      keys: ["b/4"],
-      duration: "qr",
-    }),
-    new StaveNote({
-      keys: ["C#/4", "e/4", "g/4"],
-      duration: "8",
-    })
-      .setKeyStyle(1, {
-        fillStyle: "yellow",
-      })
-      .addModifier(0, new Accidental("#")),
-    new StaveNote({
-      keys: ["C/4", "e/4", "g/4"],
-      duration: "8",
-    }).setKeyStyle(1, {
-      fillStyle: "yellow",
-    }),
-  ];
+
+  function group_by_onset(notes) {
+    var group_notes = {};
+    for (var i in notes) {
+      var note = notes[i];
+      var onset = note.onset_time;
+      group_notes[onset] = group_notes[onset] || [];
+      group_notes[onset].push(note);
+    }
+    return group_notes;
+  }
+
+  function create_notes(chords) {
+    var notes = [];
+    for (var i in chords) {
+      var chord = chords[i];
+
+      var note = new StaveNote({
+        keys: keys(chord),
+        duration: chord[0].length,
+      });
+
+      if (chord.length == 1) {
+        var n = chord[0];
+
+        if (n.note_type == "incorrect") {
+          note.setKeyStyle(0, { fillStyle: "red" });
+        } else if (n.note_type == "missing") {
+          note.setKeyStyle(0, { fillStyle: "blue" });
+        } else if (n.note_type == "extra") {
+          note.setKeyStyle(0, { fillStyle: "yellow" });
+          // I will assume that I am given the onset of the previous note
+        }
+      } else {
+        for (var j in chord) {
+          var n = chord[j];
+
+          if (n.note_type == "incorrect") {
+            note.setKeyStyle(j, { fillStyle: "red" });
+          } else if (n.note_type == "missing") {
+            note.setKeyStyle(j, { fillStyle: "blue" });
+          } else if (n.note_type == "extra") {
+            note.setKeyStyle(j, { fillStyle: "yellow" });
+            // I will assume that I am given the onset of the previous note
+          }
+        }
+      }
+      notes.push(note);
+    }
+    return notes;
+  }
+
+  function keys(chord) {
+    function format_pitch(note) {
+      var p = note.pitch_spelled;
+      return [p.slice(0, -1) + "/" + p.slice(-1)];
+    }
+    return chord.map(format_pitch).flat();
+  }
+
+  function note_by_index(idx, chords) {
+    var count = 0;
+    for (var i in chords) {
+      var chord = chords[i];
+      for (var j in chord) {
+        var note = chord[j];
+        if (count == idx) {
+          return note;
+        }
+        count++;
+      }
+    }
+  }
+
+  var chords = group_by_onset(data.notes);
+
+  var notes = create_notes(chords);
+
   // Render voice
   Formatter.FormatAndDraw(context, stave, notes);
+
   return <View>{context.render()}</View>;
 }
